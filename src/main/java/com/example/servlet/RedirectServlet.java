398b24a97dea6fa5756d5cfb5c306765f68fd679
Xai_Cheremushka <vania55ru@mail.ru>
2024-05-21 20:01:46 +0600

    Редирект

--- src/main/java/com/example/servlet/RedirectServlet.java
+++ src/main/java/com/example/servlet/RedirectServlet.java
@@ -1,4 +1,54 @@
 package com.example.servlet;
 
-public class RedirectServlet {
+import java.io.IOException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class RedirectServlet extends HttpServlet {
+    @WebServlet(name = "Main", value = "/")
+    public static class Main extends HttpServlet {
+        public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+            response.sendRedirect("index.html");
+        }
+        public void destroy() {
+        }
+    }
+
+    @WebServlet(name = "FeedBack", value = "/feedBack")
+    public static class FeedBack extends HttpServlet {
+        public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+            response.sendRedirect("feedBack.html");
+        }
+        public void destroy() {
+        }
+    }
+
+    @WebServlet(name = "Contact", value = "/contact")
+    public static class Contacts extends  HttpServlet {
+        public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+            response.sendRedirect("contact.html");
+        }
+        public void destroy() {
+        }
+    }
+
+    @WebServlet(name = "About", value = "/about")
+    public static class AboutUs extends HttpServlet {
+        public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+            response.sendRedirect("about.html");
+        }
+        public void destroy() {
+        }
+    }
+
+    @WebServlet(name = "Services", value = "/services")
+    public static class Services extends HttpServlet {
+        public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+            response.sendRedirect("services.html");
+        }
+        public void destroy() {
+        }
+    }
 }
